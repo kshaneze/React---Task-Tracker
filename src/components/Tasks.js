a06ac86efb2d0fde89a1d278175951f5b9673f20
niksa <niksaopfor@gmail.com>
2022-08-07 21:58:05 +0200

    adding comment

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,12 +2,18 @@ import Task from './Task'
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
-      <>
-   {tasks.map((task) => (
-       <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
-   ))}
-   </>
+    <>
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </>
   )
 }
 
 export default Tasks
+///
